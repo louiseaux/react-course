07774bc3ba30db735dd66aadd6fce00cb00d3bb7
Ashlyn <ashlyn.barry1@gmail.com>
2022-06-13 22:13:15 +0100

    Started work on styling

--- digital-business-card/src/App.js
+++ digital-business-card/src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer"
 
 export default function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Main />
             <Footer />
--- digital-business-card/src/components/Header.js
+++ digital-business-card/src/components/Header.js
@@ -4,13 +4,15 @@ import headshot from "../images/headshot.png"
 export default function Header() {
     return (
         <header>
-            <img src={headshot} alt="Professional headshot of Laura Smith"/>
-            <h1>Laura Smith</h1>
-            <h2>Frontend Developer</h2>
-            <h3>laurasmith.website</h3>
+            <img src={headshot} alt="Professional headshot of Laura Smith" className="header--headshot"/>
+            <h1 className="header--name">Laura Smith</h1>
+            <h2 className="header--title">Frontend Developer</h2>
+            <h3 className="header--website">laurasmith.website</h3>
 
-            <button>Email</button>
-            <button>LinkedIn</button>
+            <div className="header--buttons">
+                <button id="email-btn">Email</button>
+                <button id="linkedin-btn">LinkedIn</button>
+            </div>
         </header>
     )
 }

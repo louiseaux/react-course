5bef30796c61137d22f380401cd245622f90b473
Ashlyn <ashlyn.barry1@gmail.com>
2026-02-19 18:20:11 +1100

    display randomised multiple choice answers

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -5,25 +5,15 @@ function App() {
   const [quizStarted, setQuizStarted] = useState(false)
   const [questions, setQuestions] = useState([])
 
-  const questionElements = questions.map((q, index) => (
+  const questionElements = questions.map((question, index) => (
     <fieldset className="question-container" key={index}>
-      <legend className="question">{decode(q.question)}</legend>
-      <label>
-        <input type="radio" name={q.question} value="option-1" />
-        {decode(q.correct_answer)}
-      </label>
-      <label>
-        <input type="radio" name={q.question} value="option-2" />
-        {decode(q.incorrect_answers[0])}
-      </label>
-      <label>
-        <input type="radio" name={q.question} value="option-3" />
-        {decode(q.incorrect_answers[1])}
-      </label>
-      <label>
-        <input type="radio" name={q.question} value="option-4" />
-        {decode(q.incorrect_answers[2])}
-      </label>
+      <legend className="question">{decode(question.question)}</legend>
+      {question.answers.map((answer, index) => (
+        <label key={index}>
+          <input type="radio" name={question.question} value={answer} />
+          {decode(answer)}
+        </label>
+      ))}
     </fieldset>
   ))
 
@@ -31,24 +21,29 @@ function App() {
     setQuizStarted(true)
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple")
       .then(res => res.json())
-      .then(data => setQuestions(data.results))
+      .then(data => setQuestions(data.results.map(question => {
+        const answers = [...question.incorrect_answers]
+        const randomIndex = Math.floor(Math.random() * (answers.length + 1))
+        answers.splice(randomIndex, 0, question.correct_answer)
+        return { ...question, answers: answers }
+      })))
   }
 
   function renderQuiz() {
-    if (!quizStarted) {
+    if (quizStarted) {
+      return (
+        <form>
+          {questionElements}
+          <button>Check answers</button>
+        </form>
+      )
+    } else {
       return (
         <header>
           <h1>Quizzical</h1>
           <p>Some description if needed</p>
           <button onClick={startQuiz}>Start quiz</button>
         </header>
-      )
-    } else {
-      return (
-        <form>
-          {questionElements}
-          <button>Check answers</button>
-        </form>
       )
     }
   }
